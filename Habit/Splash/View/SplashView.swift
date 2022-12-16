0b0901f8380fa34ffa899aac94d01a57696ef5b5
joff <gsjoff@gmail.com>
2022-12-16 16:56:14 -0300

    add view and model

---
+++ Habit/Splash/View/SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  Habit
+//
+//  Created by Joff on 16/12/22.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
