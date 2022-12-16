95dd29378b24c92622791e57e3d20ebd95c70d0a
joff <gsjoff@gmail.com>
2022-12-16 16:42:59 -0300

    first commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Joff on 15/12/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
